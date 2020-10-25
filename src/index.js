287cd6d6def889c7f0f5dd62737ee7859a45e299
the-vampiire <vampiirecodes@gmail.com>
2020-10-25 16:38:04 -0500

    handlers: implement handlers using in-memory db

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
 const express = require("express");
 const corsMiddleware = require("cors");
 
+const db = require("./db");
+
+const { PORT = 8008 } = process.env;
+const { API_ORIGIN = `http://localhost:${PORT}` } = process.env;
+
 const api = express();
 
-const reflectPath = (req, res) => res.send(req.baseUrl);
+const getNote = (req, res) => {
+  const mapNote = db.findMapNote(req.noteId);
 
-const getNote = reflectPath;
-const createNote = reflectPath;
-const removeNote = reflectPath;
-const getNoteFeatures = reflectPath;
-const updateNoteFeatures = reflectPath;
-const getNotesCollection = reflectPath;
+  if (mapNote) return res.json(mapNote);
+  res.sendStatus(404);
+};
+
+const createNote = (req, res) => {
+  const newMapNote = db.saveMapNote(req.body);
+  res
+    .status(201)
+    .header("Location", `${API_ORIGIN}/notes/${newMapNote.id}`)
+    .json(newMapNote);
+};
+
+const removeNote = (req, res) => {
+  const success = db.removeMapNote(req.noteId);
+  const status = success ? 204 : 404;
+
+  res.sendStatus(status);
+};
+
+const getNoteFeatures = (req, res) => res.json(db.findFeatures(req.noteId));
+
+const updateNoteFeatures = (req, res) => {
+  const { body, noteId } = req;
+
+  db.saveFeatures(noteId, body);
+
+  res.status(201).header("Location", `${API_ORIGIN}/notes/${noteId}/features`);
+};
+
+const getNotesCollection = (_, res) => res.json(db.findMapNotes());
 
 const noteFeaturesController = express.Router({ mergeParams: true });
 noteFeaturesController
@@ -19,6 +49,14 @@ noteFeaturesController
   .put(express.json(), updateNoteFeatures);
 
 const noteController = express.Router({ mergeParams: true });
+const ensureNoteExists = (req, res, next) => {
+  const { noteId } = req.params;
+  if (!db.mapNoteExists(noteId)) return res.sendStatus(404);
+
+  req.noteId = noteId;
+  next();
+};
+noteController.use(ensureNoteExists);
 noteController.route("/").get(getNote).delete(removeNote);
 
 const notesController = express.Router({ mergeParams: true });
@@ -27,7 +65,8 @@ notesController
   .get(getNotesCollection)
   .post(express.json(), createNote);
 
-api.use(corsMiddleware({ origin: "*" }));
+api.use(corsMiddleware());
+
 api.use("/notes", notesController);
 api.use("/notes/:noteId", noteController);
 api.use("/notes/:noteId/features", noteFeaturesController);
@@ -38,5 +77,4 @@ const errorHandler = (error, req, res, next) => {
 };
 api.use(errorHandler);
 
-const { PORT = 8008 } = process.env;
 api.listen(PORT, (err) => console.log(err || `API listening on ${PORT}`));
